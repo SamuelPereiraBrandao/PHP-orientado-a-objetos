3609b735e5dc03e2c00beae85df6399164552bf3
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2023-04-25 10:19:18 -0300

    car & motorbike

--- heranca.php
+++ heranca.php
@@ -16,24 +16,28 @@ class Veiculo {
 }
 
 class Carro extends Veiculo {
-    
+    public function ligarLimpador (){
+        echo "Limpando vidro! <br>";
+    }
 }
 
 class Moto extends Veiculo {
-
+    public function AcelerarMoto(){
+        echo "Acelerando moto!";
+    }
 }
 
-
+echo"Carro: <br>";
 $carro = new Carro();
 $carro -> modelo = 'Gol';
 $carro -> cor = 'Verde';
 $carro -> ano = '2023';
-
+$carro -> ligarLimpador();
 var_dump($carro);
 echo "<br>";
 echo "O carro ".$carro->Andar()."";
 echo "<br>";
-
+echo"<hr>Moto: <br>";
 $moto = new Moto();
 $moto -> modelo = "Honda Biz";
 $moto -> cor = 'Vermelho';
@@ -41,3 +45,5 @@ $moto -> ano = '2023';
 $moto->Parar();
 echo "<br>";
 var_dump($moto);
+echo "<br>";
+$moto->AcelerarMoto();
