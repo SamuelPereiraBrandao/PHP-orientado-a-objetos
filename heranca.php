6a6d8fa6e93770c7d786c8bc8193a975a2caceea
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2023-04-25 10:02:23 -0300

    moto & carro heranca

--- heranca.php
+++ heranca.php
@@ -1,27 +1,43 @@
 <?php
 
-class Carro {
+class Veiculo {
     public $modelo;
     public $cor;
     public $ano;
 
     public function Andar(){
-
+        echo "Andou!";
     }
-
+    
     public function Parar(){
-
+        echo "Parou!";
     }
+
 }
 
-class Moto {
-    public $modelo;
-    public $cor;
-    public $ano;
-    public function Andar(){
-
-    }
-    public function Parar(){
-
-    }
+class Carro extends Veiculo {
+    
 }
+
+class Moto extends Veiculo {
+
+}
+
+
+$carro = new Carro();
+$carro -> modelo = 'Gol';
+$carro -> cor = 'Verde';
+$carro -> ano = '2023';
+
+var_dump($carro);
+echo "<br>";
+echo "O carro ".$carro->Andar()."";
+echo "<br>";
+
+$moto = new Moto();
+$moto -> modelo = "Honda Biz";
+$moto -> cor = 'Vermelho';
+$moto -> ano = '2023';
+$moto->Parar();
+echo "<br>";
+var_dump($moto);
