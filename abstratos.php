abbe10d6db2f81885c1ce3815ee76981636ab724
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2023-05-04 10:12:42 -0300

    add

--- abstratos.php
+++ abstratos.php
@@ -3,28 +3,45 @@ abstract class Banco {
     protected $saldo;
     protected $limiteSaque;
     protected $juros;
+    public function setSaldo($s){
+        $this->saldo = $s;
+    }
 
-    abstract protected function Sacar();
-    abstract protected  function Depositar();
+    public function getSaldo(){
+        return $this->saldo;
+    }
+
+    abstract protected function Sacar($s);
+    abstract protected  function Depositar($d);
 
 }
 
 class Itau extends Banco {
-        public function Sacar(){
-            echo"sacou";
-        }
-        public function Depositar(){
-            echo "depositou";
-        }
+    public function Sacar($s){
+        $this->saldo -= $s;
+        echo "<hr> Sacou:".$s;
+    }
+    public function Depositar($d){
+        $this->saldo += $d;
+        echo "<hr> Depositou:". $d;
+    }
 }
 
 class Bradesco extends Banco {
-    public function Sacar(){
-        echo"sacou";
+    public function Sacar($s){
+        $this->saldo -= $s;
+        echo "<hr> Sacou:".$s;
     }
-    public function Depositar(){
-        echo "depositou";
+    public function Depositar($d){
+        $this->saldo += $d;
+        echo "<hr> Depositou:".$d;
     }
 }
 
 $itau = new Itau();
+$itau->setSaldo(1000);
+echo"<hr> Saldo atual:".$itau->getSaldo();
+$itau->Sacar(250);
+echo"<hr> Saldo atual:".$itau->getSaldo();
+$itau->Depositar(255);
+echo"<hr> Saldo atual:".$itau->getSaldo();
